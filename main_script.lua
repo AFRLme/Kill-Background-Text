424e64ca7ae42a014c40f4ad96d238107ca59722
Lee Clarke <AFRLme@users.noreply.github.com>
2014-03-30 15:28:34 +0200

    Create main_script.lua

---
+++ main_script.lua
@@ -0,0 +1,40 @@
+--[[
+Kill background text (multiple options) [v3] (30/02/2014)
+Written by AFRLme [Lee Clarke]
+-- + --
+alternatingfrequencies@hotmail.com | skype @ AFRLme
+-- + --
+This script is donation optional. In game credit is non-negotiable.
+You are free to: ¹ use it in your game(s). ² modify the script.
+Do not remove - or edit - this comment block.
+--]]
+
+-- * tables * --
+txt = {} -- create table
+txt["_temporary_"] = "" -- set table as temporary
+
+-- * function which kills active background text (1 = all, 2 =  all characters, 3 = current character) * --
+function killText(val)
+ for i = table.maxn(txt), 1, -1 do
+  if val == 1 and txt[i]:getBool(VTextActive) then txt[i]:setValue(VTextActive, false) -- all background text
+  elseif val == 2 and txt[i]:getBool(VTextActive) and txt[i]:getLink(VTextOwner):getId().tableId == eCharacters then txt[i]:setValue(VTextActive, false) -- all character background text
+  elseif val == 3 and txt[i]:getBool(VTextActive) and txt[i]:getLink(VTextOwner):getName() == game:getLink(VGameCurrentCharacter):getName() then txt[i]:setValue(VTextActive, false); break -- current character background text
+  end
+ end
+end
+
+-- * on text display function * --
+function hText(text)
+ if text:getBool(VTextBackground) then table.insert(txt, text) end
+ -- * --
+ return false -- prevent text from being repositioned
+end
+
+-- * function for text finished * --
+function eText(text)
+ for i = table.maxn(txt), 1, -1 do if not txt[i]:getBool(VTextActive) then table.remove(txt, i) end end -- if text is inactive, remove from txt table
+end
+
+-- * event handlers * --
+registerHookFunction("setTextPosition", "hText") -- event handler for displayed text
+registerEventHandler("textStopped", "eText") -- event handler for finished text
